2ed640f78b0b4a069f6726b95a8cf436080c7462
goatess <107187023+goatess@users.noreply.github.com>
2022-07-19 11:26:30 +0200

    basic password validation system

--- src/passwordvalidation/PasswordValidation.java
+++ src/passwordvalidation/PasswordValidation.java
@@ -3,68 +3,63 @@ package passwordvalidation;
 public class PasswordValidation {
 
     public boolean isValid(String PASSWORD) {
-
-        if (isLargerThanEightChar(PASSWORD)) {
-            
+        if (isLargerThanEightChar(PASSWORD) && hasCapitalLetter(PASSWORD) && hasLowerLetter(PASSWORD)
+                && hasUnderscore(PASSWORD) && hasNumber(PASSWORD)) {
             return true;
-        } 
-        return false;
+        } else
+            return false;
     }
 
-    public boolean isLargerThanEightChar(String EIGHT_CHAR_PASSWORD) {
-        if (EIGHT_CHAR_PASSWORD.length()>8) {
+    public boolean isLargerThanEightChar(String PASSWORD) {
+        if (PASSWORD.length() > 8) {
             return true;
         }
         return false;
     }
 
-    public String[] splitPassword(String PASSWORD){
+    public String[] splitPassword(String PASSWORD) {
         String[] splitPassword = PASSWORD.split("");
         return splitPassword;
     }
 
-    public boolean hasCapitalLetter(String CAPITAL_LETTER_PASSWORD) {
-        String[] splitted = splitPassword(CAPITAL_LETTER_PASSWORD);
+    public boolean hasCapitalLetter(String PASSWORD) {
+        String[] splitted = splitPassword(PASSWORD);
         for (String character : splitted) {
-            if (character.codePointAt(0) >= 65 && character.codePointAt(0) <= 90){
+            if (character.codePointAt(0) >= 65 && character.codePointAt(0) <= 90) {
                 return true;
             }
         }
         return false;
     }
 
-    public boolean hasLowerLetter(String LOWER_LETTER_PASSWORD) {
-        String[] splitted = splitPassword(LOWER_LETTER_PASSWORD);
+    public boolean hasLowerLetter(String PASSWORD) {
+        String[] splitted = splitPassword(PASSWORD);
         for (String character : splitted) {
-            if (character.codePointAt(0) >= 97 && character.codePointAt(0) <= 122){
+            if (character.codePointAt(0) >= 97 && character.codePointAt(0) <= 122) {
                 return true;
             }
         }
         return false;
     }
 
-    public boolean hasNumber(String NUMBER_PASSWORD) {
-        String[] splitted = splitPassword(NUMBER_PASSWORD);
+    public boolean hasNumber(String PASSWORD) {
+        String[] splitted = splitPassword(PASSWORD);
         for (String character : splitted) {
-            if (character.codePointAt(0) >= 48 && character.codePointAt(0) <= 57){
+            if (character.codePointAt(0) >= 48 && character.codePointAt(0) <= 57) {
                 return true;
             }
         }
         return false;
     }
 
-    public boolean hasUnderscore(String UNDERSCORE_PASSWORD) {
-        String[] splitted = splitPassword(UNDERSCORE_PASSWORD);
+    public boolean hasUnderscore(String PASSWORD) {
+        String[] splitted = splitPassword(PASSWORD);
         for (String character : splitted) {
-            if (character.codePointAt(0) == 95){
+            if (character.codePointAt(0) == 95) {
                 return true;
             }
         }
         return false;
     }
-
-
-
-    
 
 }
--- src/passwordvalidation/passwordvalidationTest.java
+++ src/passwordvalidation/passwordvalidationTest.java
@@ -30,9 +30,9 @@ public class passwordvalidationTest {
 
     @Test
     public void dont_validate_password_when_it_doesnt_have_a_capital_letter(){
-        final String CAPITAL_LETTER_PASSWORD = "a";
+        final String WITHOUT_CAPITAL_LETTER_PASSWORD = "a";
         PasswordValidation validation = new PasswordValidation();
-        assertFalse(validation.hasCapitalLetter(CAPITAL_LETTER_PASSWORD));
+        assertFalse(validation.hasCapitalLetter(WITHOUT_CAPITAL_LETTER_PASSWORD));
     }
 
     @Test
@@ -44,9 +44,9 @@ public class passwordvalidationTest {
 
     @Test
     public void dont_validate_password_when_it_doesnt_have_a_lower_letter(){
-        final String LOWER_LETTER_PASSWORD = "A";
+        final String WITHOUT_LOWER_LETTER_PASSWORD = "A";
         PasswordValidation validation = new PasswordValidation();
-        assertFalse(validation.hasLowerLetter(LOWER_LETTER_PASSWORD));
+        assertFalse(validation.hasLowerLetter(WITHOUT_LOWER_LETTER_PASSWORD));
     }
 
     @Test
@@ -58,9 +58,9 @@ public class passwordvalidationTest {
 
     @Test
     public void dont_validate_password_when_it_doesnt_have_a_number(){
-        final String NUMBER_PASSWORD = "armario";
+        final String WITHOUT_NUMBER_PASSWORD = "armario";
         PasswordValidation validation = new PasswordValidation();
-        assertFalse(validation.hasNumber(NUMBER_PASSWORD));
+        assertFalse(validation.hasNumber(WITHOUT_NUMBER_PASSWORD));
     }
     @Test
     public void validate_password_when_it_has_an_underscore(){
@@ -71,9 +71,31 @@ public class passwordvalidationTest {
 
     @Test
     public void dont_validate_password_when_it_doesnt_have_an_underscore(){
-        final String UNDERSCORE_PASSWORD = "armario";
+        final String WITHOUT_UNDERSCORE_PASSWORD = "armario";
         PasswordValidation validation = new PasswordValidation();
-        assertFalse(validation.hasUnderscore(UNDERSCORE_PASSWORD));
+        assertFalse(validation.hasUnderscore(WITHOUT_UNDERSCORE_PASSWORD));
     }
-    
+
+    @Test
+    public void return_when_a_password_is_valid(){
+        final String VALID_PASSWORD = "A_rmari_0";
+        PasswordValidation validation = new PasswordValidation();
+        assertTrue(validation.isValid(VALID_PASSWORD));
+    }
+
+    @Test
+    public void return_when_a_password_is_invalid(){
+        final String NO_UNDERSCORE_PASSWORD = "ArmariII0";
+        final String NO_CAPITAL_PASSWORD = "a_rmari_0";
+        final String NO_LOWER_PASSWORD = "A_RMARI_0";
+        final String EIGHT_CHAR_PASSWORD = "Armari_0";
+        final String NO_NUMBER_PASSWORD = "A_rmari_O";
+
+        PasswordValidation validation = new PasswordValidation();
+        assertFalse(validation.isValid(NO_UNDERSCORE_PASSWORD));
+        assertFalse(validation.isValid(NO_CAPITAL_PASSWORD));
+        assertFalse(validation.isValid(NO_LOWER_PASSWORD));
+        assertFalse(validation.isValid(EIGHT_CHAR_PASSWORD));
+        assertFalse(validation.isValid(NO_NUMBER_PASSWORD));
+    }
 }
